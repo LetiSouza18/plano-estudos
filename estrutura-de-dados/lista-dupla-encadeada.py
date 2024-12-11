cd57dbc62456fbd76d9731cc1492a02d6a320891
Letícia Souza <138714479+LetiSouza18@users.noreply.github.com>
2024-12-11 20:45:14 -0300

    Create lista-dupla-encadeada.py

---
+++ estrutura-de-dados/lista-dupla-encadeada.py
@@ -0,0 +1,61 @@
+class No:
+    def __init__(self, data):
+        self.data = data
+        self.proximo = None
+        self.anterior = None
+
+class Lista:
+    def __init__(self):
+        self.inicio = None
+        self.fim = None
+        self.__tamanho = 0
+
+    def vazia(self):
+        return self.inicio is None
+
+    def add_inicio(self, data):
+        # Implementação para adicionar no início
+
+    def add_fim(self, data):
+        novoNo = Node(data)
+        if self.inicio is None:
+          self.inicio = novoNo
+        else:
+          self.fim = novoNo
+
+    def inserir(self, i, data):
+        novoNo = Node(data)
+        if i == 0:
+          if self.inicio is None:
+            self.inicio = self.fim = novoNo
+          else:
+            novoNo.proximo = self.inicio
+            self.inicio.anterior = novoNo
+            self.inicio = novoNo
+        else:
+          atual = self.inicio
+          index = 0
+          while atual is not None and index < i:
+            atual = atual.proximo
+            index += 1
+          if atual is None:
+            self.add_fim(data)
+          else:
+            anterior = atual.anterior
+            anterior.proximo = novoNo
+            novoNo.anterior = anterior
+            novoNo.proximo = atual
+            atual.anterior = novoNo
+
+    def remover_inicio(self):
+        # Implementação para remover do início
+
+    def remover_fim(self):
+        # Implementação para remover do fim
+
+    # ... outros métodos (buscar, etc.) ...
+
+# Exemplo de uso:
+minha_lista = Lista()
+minha_lista.add_inicio(10)
+minha_lista.add_fim(20)
